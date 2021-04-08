641b09746044dbabd695018a7266500994b6600a
Manuel Schenkel <manuel.schenkel@gmail.com>
2021-04-09 00:03:42 +0200

    add Aufgaben 5-9

--- JavaBasics/src/AufgabenSerieA.java
+++ JavaBasics/src/AufgabenSerieA.java
@@ -37,5 +37,39 @@ public class AufgabenSerieA {
         double reisezeit = distanz / geschwindigkeit;
         System.out.println("Reisezeit: " + reisezeit);
 
+        //Aufgabe 5 Brüche multiplizieren
+        int zaehler1 = 3, nenner1 = 4;
+        int zaehler2 = 5, nenner2 = 6;
+        int zaehler3 = zaehler1 * zaehler2;
+        int nenner3 = nenner1 * nenner2;
+        System.out.println("Bruch: " + zaehler3 + "/" + nenner3);
+
+        //Aufgabe 6 Brüche dividieren
+        int zaehler4 = zaehler1 * nenner2;
+        int nenner4 = nenner1 * zaehler2;
+        System.out.println("Bruch: " + zaehler4 + "/" + nenner4);
+
+        //Aufgabe 7 Monate ausgeben
+        String[] monate = {"Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"};
+        System.out.println(monate[4]);
+
+        //Aufgabe 8 Tage summieren
+        int[] tageProMonat = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int summeTage = 0;
+        for (int i = 3; i < 7; i++) {
+            summeTage = summeTage + tageProMonat[i-1];
+        }
+        System.out.println(summeTage);
+
+        //Aufgabe 9 Tage seit Zeitrechnung
+        int tag = 7, monat = 10, jahr = 2016;
+        int tageJahre = (jahr-1) * 365;
+        int tageMonate = 0;
+        for (int i = 0; i < monat-1; i++) {
+            tageMonate = tageMonate + tageProMonat[i];
+        }
+        summeTage = tageJahre + tageMonate + tag;
+        System.out.println(summeTage);
+
     }
 }
